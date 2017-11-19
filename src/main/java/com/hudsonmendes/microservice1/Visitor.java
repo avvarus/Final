7692dc5aaeb877a761738b4e346419420868b595
sujeeth <avvaru79.java@gmail.com>
2017-11-18 22:06:17 -0500

    second controller
    Signed-off-by: sujeeth <avvaru79.java@gmail.com>

--- src/main/java/com/hudsonmendes/microservice1/Visitor.java
+++ src/main/java/com/hudsonmendes/microservice1/Visitor.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Visitor {
 
-
     @RequestMapping(path = "/first")
     public String greeting() {
 
         return "Hello babyy !!!";
+    }
+
+    @RequestMapping(path = "/second")
+    public String second() {
+
+        return "Hello babyy second!!!";
     }
 
 }
